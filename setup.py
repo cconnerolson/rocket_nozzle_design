a0b8ff6b80f62abfcf937c65a831ddf93405e7a4
cconnerolson <43979104+cconnerolson@users.noreply.github.com>
2019-03-29 21:07:08 -0500

    periodic commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rocket_nozzle_design',
+      version='0.1',
+      description='Tools fort rocket engine nozzle design.',
+      url='https://github.com/cconnerolson/a5_p3.git',
+      author='Conner Olson',
+      author_email='cconnerolson@gmail.com',
+      license='MIT',
+      packages=['pandas', 'matplotlib', 'numpy'],
+      zip_safe=False
+      )
