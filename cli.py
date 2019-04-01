f7a4a085e62aa393d481e817ad4d203ff91771b2
cconnerolson <43979104+cconnerolson@users.noreply.github.com>
2019-04-01 02:22:17 -0500

    initalize command line interface file

--- cli.py
+++ cli.py
@@ -2,8 +2,38 @@ import click
 from isentropic_flow import *
 from specific_heat import specific_heat
 
+print('\nUsage:')
 
-@click.command()
+print('  python3 cli.py <command>\n\n')
+
+print('Tools:\n')
+
+
+print('           Input arg:           Output arg:\n')
+
+print('  -m2p  -  Mach number          Pressure Ratio')
+print('  -m2t  -  Mach number          Temperature Ratio')
+print('  -m2a  -  Mach number          Area Ratio')
+print('  -p2m  -  Pressure Ratio       Mach Number')
+print('  -p2t  -  Pressure Ratio       Temperature Ratio')
+print('  -p2a  -  Pressure Ratio       Area Ratio')
+print('  -t2m  -  Temperature Ratio    Mach Number')
+print('  -t2p  -  Temperature Ratio    Pressure Ratio')
+print('  -t2a  -  Temperature Ratio    Area Ratio')
+print('  -a2m  -  Area Ratio           Mach Number')
+print('  -a2p  -  Area Ratio           Pressure Ratio')
+print('  -a2t  -  Area Ratio           Mach Number\n')
+
+print('  -cp   -  Temperature          Specific Heat (Cp)')
+
+
+''''print('Please select an option...\n')
+print('    [1] - calculate specific heat')
+print('    [2] - isentropic flow table conversions')
+print('    [')'''
+
+
+''''@click.command()
 @click.option("--count", default=1, help="Number of greetings.")
 @click.option("--name", prompt="Your name",
               help="The person to greet.")
@@ -13,4 +43,4 @@ def hello(count, name):
         click.echo("Hello, %s!" % name)
 
 if __name__ == '__main__':
-    hello()
+    hello()'''
--- isentropic_flow.py
+++ isentropic_flow.py
@@ -1,81 +1,121 @@
 import numpy as np
-import csv
+import pandas
+
+data = pandas.read_csv('data/isentropic_flow_table.csv')  # , names=colnames)
+
+M_data = data.Ma.tolist()
+P_data = data.p.tolist()
+T_data = data.t.tolist()
+A_data = data.A.tolist()
 
 
-with open('data/isentropic_flow_table.csv', 'r') as f:
-	reader = csv.reader(f)
-	next(reader)  # skip header
-	i_f_data = [r for r in reader]
+# print(A_data)
 
-
-def M2P(Ma):  # 1 output todo description
+def m2p(M):  # 1 output todo description
 	"""
 	*** description ***
 	:param Ma: Mach number
 	"""
-	if Ma < 0 or Ma >= 6:
+	if M < 0 or M >= 6:
 		raise ValueError('Mach number input outside valid domain')
-	return np.interp(Ma, i_f_data[:, 0], i_f_data[:, 1])
+	return np.interp(M, M_data, P_data)
 
 
-def M2T(Ma):  # 1 output todo description
+def m2t(M):  # 1 output todo description
 	"""
 	*** description ***
 	:param Ma: Mach number
 	"""
-	if Ma < 0 or Ma >= 6:
+	if M < 0 or M >= 6:
 		raise ValueError('Mach number input outside valid domain')
-	return np.interp(Ma, i_f_data[:, 0], i_f_data[:, 2])
+	return np.interp(M, M_data, T_data)
 
 
-def M2A(Ma):  # 1 output todo description
+def m2a(M):  # 1 output todo description
 	"""
 	*** description ***
 	:param Ma: Mach number
 	"""
-	if Ma < 0 or Ma >= 6:
+	if M < 0 or M >= 6:
 		raise ValueError('Mach number input outside valid domain')
-	return np.interp(Ma, i_f_data[:, 0], i_f_data[:, 5])
+	return np.interp(M, M_data, A_data)
 
 
-def P2M(P):  # 1 output # todo P2M, description
-	pass
+print(m2a(0.05))
+
+
+def p2m(P):  # 1 output # todo fix P2M, description
+	"""
+	*** description ***
+	:param P: P/P_0 pressure ratio
+	"""
+	if P < 9.746e-4 or P > 1:
+		raise ValueError('Pressure ratio input outside valid domain')
+	return np.interp(P, P_data, M_data)
+
+
+def p2t(P):  # 1 output todo P2T, description
 	"""
 	*** description ***
 	:param P: P/P_0 pressure ratio
 	"""
 	if P < 0 or P >= 6:
-		raise ValueError('*** input outside valid domain')
-	return np.interp(P, i_f_data[:, 1], i_f_data[:, 0])
-
-
-def P2T(P):  # 1 output todo P2T, description
+		raise ValueError('Pressure ratio input outside valid domain')
 	pass
 
 
-def P2A(P):  # 1 output todo P2A, description
+def p2a(P):  # 1 output todo P2A, description
+	"""
+	*** description ***
+	:param P: P/P_0 pressure ratio
+	"""
 	pass
 
 
-def T2M(P):  # 1 output todo T2M, description
+def t2m(T):  # 1 output todo T2M, description
+	"""
+	*** description ***
+	:param T: T/T_0 temperature ratio
+	"""
 	pass
 
 
-def T2P(P):  # 1 output todo T2P, description
+def t2p(T):  # 1 output todo T2P, description
+	"""
+	*** description ***
+	:param T: T/T_0 temperature ratio
+	"""
 	pass
 
 
-def T2A():  # 1 output todo T2A, description
+def t2t(T):  # 1 output todo T2A, description
+	"""
+	*** description ***
+	:param T: T/T_0 temperature ratio
+	"""
 	pass
 
 
-def A2M():  # 2 outputs todo A2M, description
+def a2m(A):  # 2 outputs todo A2M, description
+	"""
+	*** description ***
+	:param A: A/A* area ratio
+	"""
 	pass
 
 
-def A2P():  # 2 output todo A2P, description
+def a2p(A):  # 2 output todo A2P, description
+	"""
+	*** description ***
+	:param A: A/A* area ratio
+	"""
 	pass
 
 
-def A2T():  # 2 outputs todo A2T, description
+def a2t(A):  # 2 outputs todo A2T, description
+	"""
+	*** description ***
+	:param A: A/A* area ratio
+	"""
 	pass
+
