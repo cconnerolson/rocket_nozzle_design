bfda17c2bdb511d429ba55d1c09a5c7ef031391d
Conner Olson <43979104+cconnerolson@users.noreply.github.com>
2019-04-07 21:44:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='rocket_nozzle_design',
       version='0.1',
-      description='Tools fort rocket engine nozzle design.',
+      description='Tools for rocket engine nozzle design.',
       url='https://github.com/cconnerolson/a5_p3.git',
       author='Conner Olson',
       author_email='cconnerolson@gmail.com',
